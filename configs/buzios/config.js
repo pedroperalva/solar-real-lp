be05fe88c646e6ffbca2848f3cf36053501ad0ff
Pedro Peralva <pedro.peralva@nubank.com.br>
2026-03-17 11:17:33 -0300

    feat: buzios

---
+++ configs/buzios/config.js
@@ -0,0 +1,27 @@
+export default {
+  name: "Espelho das Águas Búzios",
+  slug: "buzios",
+  localeDefault: "pt",
+  author: "Leco Biagioni",
+  assets: {
+    heroBanner: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/LPs/buzios/buzios.png`,
+    vectorBackground: "/vector.png",
+    featuredImage: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/Videos%20lps/cristo.mp4`,
+  },
+  imagesArray1: [
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/LPs/buzios/2026-03-03 at 07.42.35.jpeg`,
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/LPs/buzios/3-03 at 07.42.35.jpeg`,
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/LPs/buzios/buz.jpeg`,
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/LPs/buzios/buzios.jpeg`,
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/LPs/buzios/buzo.jpeg`,
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/LPs/buzios/buzz.jpeg`,
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/LPs/buzios/e 2026-03-03 at 07.42.35.jpeg`,
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/LPs/buzios/gsgfs.jpeg`,
+  ],
+  imagesArray2: [
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/LPs/buzios/gsgsgsg.jpeg`,
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/LPs/buzios/gss.jpeg`,
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/LPs/buzios/gsts.jpeg`,
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/LPs/buzios/Image .jpeg`,
+  ],
+};
